2df513db41cd0dde9748cd43a71c67e65c4dffc0
Jordy Pena <jordypenamontes@gmail.com>
2021-08-09 21:25:50 -0500

    landingpage/design --navabar styles changed --home.js card created w button

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -4,7 +4,12 @@ const Home = () => {
   return (
     <main className='home-container'>
       <section className='home-background-img'>
-
+        <h1 className='sasquatch-header'>Sasquatch</h1>
+        <h2 className='beer-me-header'>Beer Me</h2>
+        <section className='home-card'>
+          <h3 className='home-card-header'>Find your path</h3>
+          <button className='home-card-button'>Grrrrr</button>
+        </section>
       </section>
     </main>
   )
