8c8f3cf015f76cc603f115bec28528a1ed0c9c36
Jordy Pena <67558072+JordyPena@users.noreply.github.com>
2021-08-16 22:12:53 -0500

    state-input (#9)
    -- created 2nd input
    -- styled 2nd input
    -- added button to allow 2 inputs
    --added input verification

--- src/Searchbar/Searchbar.js
+++ src/Searchbar/Searchbar.js
@@ -3,40 +3,63 @@ import { useState } from "react";
 import Result from "../Results/Result";
 const Searchbar = () => {
   const [breweries, setBreweries] = useState([]);
-  const [userInput, setUserInput] = useState("");
+  const [cityInput, setCityInput] = useState("");
+  const [stateInput, setStateInput] = useState("");
+  const [ error, setError] = useState(false);
 
   const inputChanged = (e) => {
-    setUserInput(e.target.value);
+    const {name, value} = e.target;
+    if (name === 'city') {
+      setCityInput(value);
+    } else if (name === 'state') {
+      setStateInput(value);
+    } 
   };
 
   const brewerySubmitted = (e) => {
     e.preventDefault();
+    if (!cityInput && !stateInput) {
+      return setError(true);
+    } else 
+    setError(false);
     fetch(
-      `https://api.openbrewerydb.org/breweries?by_city=${userInput}&by_state=Texas`
+      `https://api.openbrewerydb.org/breweries?by_city=${cityInput}&by_state=${stateInput}`
     )
       .then((response) => response.json())
       .then((data) => {
         console.log("my result", data);
         setBreweries(data);
       });
-  };
-
-  return (
-    <>
+    };
+    
+    return (
+      <>
       <form
         className="searchbar-form-styles"
         onSubmit={(e) => brewerySubmitted(e)}
-      >
+        >
         <label className="searchbar-label">
+          {error === true && <h4 className='error-styles'>Fill out all input fields</h4>}
           <span>Enter City Name</span>
           <input
             type="text"
-            name="searchbar"
-            value={userInput}
+            name="city"
+            value={cityInput}
             onChange={(e) => inputChanged(e)}
             placeholder="Dallas"
-          />
+            />
         </label>
+        <label className="searchbar-label">
+          <span>Enter State Name</span>
+          <input
+            type="text"
+            name="state"
+            value={stateInput}
+            onChange={(e) => inputChanged(e)}
+            placeholder="Texas"
+            />
+        </label>
+        <button className='searchbar-form-styles'>Submit</button>
       </form>
       <section className="breweries-grp">
         {breweries.map((brewery, index) => {
