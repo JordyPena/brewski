b0cf38c0e46557ead996fa5ba1675deeac658075
Jordy Pena <jordypenamontes@gmail.com>
2021-08-16 22:20:06 -0500

    header-link --imported Link from react-router --styled link

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,13 +1,15 @@
-import '../Navbar/Navbar.css';
-import Menu from '../Menu/Menu';
+import "../Navbar/Navbar.css";
+import Menu from "../Menu/Menu";
+import { Link } from "react-router-dom";
 const Navbar = () => {
-
   return (
-    <header className='header-container'>
-      <h2>Sasquatch</h2>
+    <header className="header-container">
+      <Link to="/" className='header-link-styles'>
+        <h2>Sasquatch</h2>
+      </Link>
       <Menu />
     </header>
-  )
-}
+  );
+};
 
 export default Navbar;
